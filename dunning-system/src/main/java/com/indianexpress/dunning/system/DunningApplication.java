0baf27e63db5dad03549a375a6df352d92193730
Maitry Lohani <49520756+matiry321@users.noreply.github.com>
2023-11-30 11:21:56 +0530

    Add files via upload

---
+++ dunning-system/src/main/java/com/indianexpress/dunning/system/DunningApplication.java
@@ -0,0 +1,13 @@
+package com.indianexpress.dunning.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunningApplication.class, args);
+	}
+
+}
---
+++ dunning-system/src/test/java/com/indianexpress/dunning/system/DunningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indianexpress.dunning.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
